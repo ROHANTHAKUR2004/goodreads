39dd2af782eb10cd77f152c29e23ad021f121f55
rohan <rohanthakur89768@gmail.com>
2024-07-26 11:49:13 +0530

    added eslint config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve :{
+    alias :{
+        src: "/src",
+        Components : "/src/Components",
+        Pages : "/src/Pages",
+        Assets : "/src/Assets",
+        Layouts : "/src/Layouts",
+        Redux : "/src/Redux",
+        Routes : "/src/Routes",
+        Helpers : "/src/Helpers",
+        Configs : "/src/Configs"
+    }
+  },
+});
