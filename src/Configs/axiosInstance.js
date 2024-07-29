1863b58d7d88e29c95574ba5997d72d22b368d07
rohan <rohanthakur89768@gmail.com>
2024-07-29 16:15:57 +0530

    Completed

--- src/Configs/axiosInstance.js
+++ src/Configs/axiosInstance.js
@@ -1,6 +1,6 @@
 import { default as axios } from "axios";
 
 const instance = axios.create();
-instance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3005/api/v1";
+instance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3007/api/v1";
 
 export default instance;
